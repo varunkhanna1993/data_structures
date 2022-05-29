6239d02e3ced0128b715a1ac0606445ccf04c0e1
varunkhanna1993 <varunkhanna93@hotmail.com>
2022-05-29 18:25:21 +1200

    stacks updated

---
+++ 5. Stacks and Queues/stacks_constructor.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value ):
+        new_node = Node(value)
+        self.top =  new_node
+        self.height  =1
+    
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top 
+            self.top = new_node
+        self.height +=1  
+        return True
+    
+    def pop(self):
+        if self.height == 0:
+            return None
+
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -=1
+        return temp
+
+    def print_stack(self):
+        temp = self.top
+        while temp:
+            print(temp.value)
+            temp = temp.next
+        print("the height of the stack is:", self.height)
+
+my_stack = Stack(4)
+my_stack.push(56)
+my_stack.push(60)
+my_stack.pop()
+my_stack.pop()
+my_stack.pop()
+my_stack.print_stack()
+# print(my_stack.top.next.value)
+
