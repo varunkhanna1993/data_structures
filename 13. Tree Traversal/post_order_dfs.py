3f1224f62c1f574e65899646da73d63943c48bab
varunkhanna1993 <varunkhanna93@hotmail.com>
2022-06-03 01:35:08 +1200

    depth first post order, pre order and in order completed. Solid foundation to try leetcode now.

---
+++ 13. Tree Traversal/post_order_dfs.py
@@ -0,0 +1,131 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        """_summary_
+        steps involved:
+        1. First create a new node
+        2. then check if root node is none just insert 
+        the node there
+        3. temp = self.root (this would be used to traverse through the tree)
+        4. while loop (as we dont know how many times we are going to run through)
+        if value = node value then return false (edge case)
+        if value < node then left otherwise right
+        if value is empty then insert there
+        Args:
+            value (_type_): _description_
+        """
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        # you dont need the following two lines of code as if its empty,
+        #  it wouldn't run the while loop as it will be pointing to None
+        # if self.root is None:
+        #     return False
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+    def min_value_node(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node
+
+    def max_value_node(self, current_node):
+        while current_node.right is not None:
+            current_node = current_node.right
+        return current_node
+
+
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root) # instantiate the traverse func for recursion
+        return results
+
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right) 
+        traverse(self.root)
+        return results
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.dfs_in_order())
+
+
+
+
+
